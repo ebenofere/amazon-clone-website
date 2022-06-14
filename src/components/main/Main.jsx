a44faca1a8c79ff4a76d06274fa14685abdd78e5
ebenofere <eofere@gmail.com>
2022-06-14 01:25:49 +0100

    update onthe product boxes

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,42 +1,18 @@
-import { Stack, HStack, VStack, Box } from '@chakra-ui/react'
-
-const boxCategory = [
-    {
-        id: 1, categoryTitle: "Keep shopping for"
-    },
-    {
-        id: 2, categoryTitle: "Shop by Category"
-    },
-    {
-        id: 3, categoryTitle: "Get fit at home"
-    },
-    {
-        id: 4, categoryTitle: "Shop Father's Day Gifts"
-    },
-    {
-        id: 5, categoryTitle: "Beauty picks"
-    },
-    {
-        id: 6, categoryTitle: "Deal of the day"
-    },
-    {
-        id: 7, categoryTitle: "Health & Personal care"
-    },
-    {
-        id: 8, categoryTitle: "Shop Home & Office"
-    },
-]
+import { Stack, HStack, VStack, Box, Wrap, WrapItem } from '@chakra-ui/react'
+import { boxCategory } from 'store/productboxes'
 
 const BoxRow = () => {
     return (
-        <>
+        <Wrap spacing="30px" bg="yellow.200">
             {boxCategory.map((box) => (
-                <Box key={box.id} p={5} borderWidth="1px" style={{ border: "1px solid black" }}>
-                    <h1>{box.categoryTitle}</h1>
-                </Box>
+                <WrapItem>
+                    <Box key={box.id} p={5} borderWidth="1px" w='200px' h='300px' style={{ border: "1px solid blue" }}>
+                        <h1>{box.categoryTitle}</h1>
+                    </Box>
+                </WrapItem>
             ))
             }
-        </>
+        </Wrap>
     )
 }
 
---
+++ src/store/productboxes.jsx
@@ -0,0 +1,26 @@
+export const boxCategory = [
+    {
+        id: 1, categoryTitle: "Keep shopping for"
+    },
+    {
+        id: 2, categoryTitle: "Shop by Category"
+    },
+    {
+        id: 3, categoryTitle: "Get fit at home"
+    },
+    {
+        id: 4, categoryTitle: "Shop Father's Day Gifts"
+    },
+    {
+        id: 5, categoryTitle: "Beauty picks"
+    },
+    {
+        id: 6, categoryTitle: "Deal of the day"
+    },
+    {
+        id: 7, categoryTitle: "Health & Personal care"
+    },
+    {
+        id: 8, categoryTitle: "Shop Home & Office"
+    },
+]
